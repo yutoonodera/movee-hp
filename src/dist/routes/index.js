22c9fdcc3bdb25588b76f9d2969df89bb49080ac
onody <onoderayuto@onody00.local>
2023-12-25 15:52:36 +0900

    mvr削除、blog表示

--- src/dist/routes/index.js
+++ src/dist/routes/index.js
@@ -1,9 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const indexcontroller_1 = require("../controllers/indexcontroller");
-const analyticscontroller_1 = require("../controllers/analyticscontroller");
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-router.get('/', indexcontroller_1.handleIndexGet);
-router.post('/analytics', analyticscontroller_1.handleAnalyticsPost);
+router.get("/", indexcontroller_1.handleIndexGet);
 module.exports = router;
