cfab643e6065a05485b70cc1c8ef6775446ba116
onody <onoderayuto@onody00.local>
2024-01-31 16:57:24 +0900

    データ二次利用ページの追加

---
+++ src/dist/controllers/subpagecontroller.js
@@ -0,0 +1,35 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.handleSubPageGet = void 0;
+const handleSubPageGet = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { Profile } = require("../models/Profile");
+    try {
+        // // Profile クラスのインスタンスを作成
+        // const profile = new Profile();
+        // const profileData = await profile.getCheckedProfileData();
+        // const currentDate = new Date();
+        // currentDate.setMonth(currentDate.getMonth() - 1); // 1ヶ月前の日付を取得
+        // const year = currentDate.getFullYear();
+        // const month = (currentDate.getMonth() + 1).toString().padStart(2, "0");
+        // const formattedLastMonth = year + "-" + month;
+        // // デバッグ情報をコンソールに出力
+        // レンダリング時に Github データをテンプレートに渡す
+        res.render("secondary-use-data", {
+            title: "株式会社movee",
+        });
+    }
+    catch (error) {
+        // エラーハンドリング
+        console.error("エラー:", error);
+    }
+});
+exports.handleSubPageGet = handleSubPageGet;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,11 @@
 import { handleIndexGet } from "../controllers/indexcontroller";
+import { handleSubPageGet } from "../controllers/subpagecontroller";
 
 var express = require("express");
 var router = express.Router();
 
 router.get("/", handleIndexGet);
+
+router.get("/secondary-use-data", handleSubPageGet);
 
 module.exports = router;
